6efd651045730b92a8767e10f3feeed28bfffd52
Hamid Nikbakht <me@nikleotide.com>
2018-04-21 01:10:19 -0400

    Added merge before intersection

--- AfterSegmentation.sh
+++ AfterSegmentation.sh
@@ -6,7 +6,8 @@ bedtools merge -d 10 -i Parental_H3K36me2-50kb-H3K36me2-NonGenic-NOT-MERGED.bedg
 
 
 
-#
+####################################################################################################
+
 ## In case of merging before intersectioning with genic/intergenic bed files
 ## First adding the length of each segment and filtering out those below 0.5 and smaller than 50 (trailing ones)
 awk '{if (($3-$2)/1000 > 50 && $4 > 0.5) print $0"\t"($3-$2)/1000}' ../4252_4295_C3H10T1_Parental_H3K36me2/Segmentation_50/4252_4295_C3H10T1_Parental_H3K36me2-50kb-H3K36me2-1stComplete_Genome-peaks.fixed.bedgraph > ../4252_4295_C3H10T1_Parental_H3K36me2/Segmentation_50/4252_4295_C3H10T1_Parental_H3K36me2-50kb-H3K36me2-1stComplete_Genome-peaks.fixed-filtered-LENGTH.bedgraph
@@ -21,3 +22,5 @@ bedtools merge -d 10 -i ../4252_4295_C3H10T1_Parental_H3K36me2/Segmentation_50/4
 bedtools intersect -a mm10hgTables_NonGenic.bed -b Parental_H3K36me2-50kb-H3K36me2-MERGED-ALL.bedgraph -wb | cut -f 1-3,7 | awk '{print $0"\t"($3-$2)/1000}' | awk '{if ($4 > 0.5 && $5 > 50) print $0}' | sed 's/chrX/chr23/' | sed 's/chrY/chr24/' | sed 's/chr//' | sort -k1,1n -k2,2n | sed 's/^/chr/' | sed 's/chr23/chrX/' | sed 's/chr24/chrY/' > Parental_H3K36me2-50kb-H3K36me2-MERGED-NON-GENIC.bedgraph
 # And for Genic
 bedtools intersect -a mm10hgTables_Genic.bed -b Parental_H3K36me2-50kb-H3K36me2-MERGED-ALL.bedgraph -wb | cut -f 1-3,7 | awk '{print $0"\t"($3-$2)/1000}' | awk '{if ($4 > 0.5 && $5 > 50) print $0}' | sed 's/chrX/chr23/' | sed 's/chrY/chr24/' | sed 's/chr//' | sort -k1,1n -k2,2n | sed 's/^/chr/' | sed 's/chr23/chrX/' | sed 's/chr24/chrY/' > Parental_H3K36me2-50kb-H3K36me2-MERGED-GENIC.bedgraph
+
+#
