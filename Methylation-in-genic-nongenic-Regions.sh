7f0cfb25c2ff467a9f340958bdaf3f9bd159ad52
Hamid Nikbakht <me@nikleotide.com>
2018-04-30 15:43:49 -0400

    Update Methylation-in-genic-nongenic-Regions.sh

--- Methylation-in-genic-nongenic-Regions.sh
+++ Methylation-in-genic-nongenic-Regions.sh
@@ -2,4 +2,19 @@
 # /media/behnam/Black_Seagate2/Mouse/Data/mm10hgTables_Genic.bed
 # /media/behnam/Black_Seagate2/Mouse/Data/mm10hgTables_NonGenic.bed
 
+# We use these files to intersect with methylation and measure their methylation in each gene/non-gene region and compare their changes from cell to cell
 
+## Genic
+# 1) Count the number of CpGs
+bedtools intersect -a /media/behnam/Black_Seagate2/Mouse/Data/mm10hgTables_Genic.bed -b /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_3_Parental_Domains_in_4_Cells/Raw_Methylation_bedgraphs_4_Cells/Parental_C3H10T_BS_1.profile.cg_strand_combined.bedgraph -c > /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_genic-nongenic-regions_in_4_Cells/CpG-Counts/Parental_C3H10T_BS_GENIC_CpG_counts.dat
+
+# 2) Calculate the average methylation
+bedtools intersect -a /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_genic-nongenic-regions_in_4_Cells/CpG-Counts/Parental_C3H10T_BS_GENIC_CpG_counts.dat -b /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_3_Parental_Domains_in_4_Cells/Raw_Methylation_bedgraphs_4_Cells/Parental_C3H10T_BS_1.profile.cg_strand_combined.bedgraph -wa -wb | groupBy -g 1,2,3,4 -c 8 -o sum | awk '{print $0"\t"$5/$4}' > /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_genic-nongenic-regions_in_4_Cells/CpG_counts_sum_average/Parental_C3H10T_BS_GENIC_CpG_counts_sum_average.dat
+
+
+## Non-Genic
+# 1) Count the number of CpGs
+bedtools intersect -a /media/behnam/Black_Seagate2/Mouse/Data/mm10hgTables_NonGenic.bed -b /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_3_Parental_Domains_in_4_Cells/Raw_Methylation_bedgraphs_4_Cells/Parental_C3H10T_BS_1.profile.cg_strand_combined.bedgraph -c > /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_genic-nongenic-regions_in_4_Cells/CpG-Counts/Parental_C3H10T_BS_NONGENIC_CpG_counts.dat
+
+# 2) Calculate the average methylation
+bedtools intersect -a /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_genic-nongenic-regions_in_4_Cells/CpG-Counts/Parental_C3H10T_BS_NONGENIC_CpG_counts.dat -b /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_3_Parental_Domains_in_4_Cells/Raw_Methylation_bedgraphs_4_Cells/Parental_C3H10T_BS_1.profile.cg_strand_combined.bedgraph -wa -wb | groupBy -g 1,2,3,4 -c 8 -o sum | awk '{print $0"\t"$5/$4}' > /media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_genic-nongenic-regions_in_4_Cells/CpG_counts_sum_average/Parental_C3H10T_BS_NONGENIC_CpG_counts_sum_average.dat
