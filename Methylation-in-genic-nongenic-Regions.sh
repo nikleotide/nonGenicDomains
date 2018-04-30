8e683e5a543ff1c667bbbe9cc87dd20ceea393df
Hamid Nikbakht <me@nikleotide.com>
2018-04-30 15:19:25 -0400

    Create Methylation-in-genic-nongenic-Regions.sh

---
+++ Methylation-in-genic-nongenic-Regions.sh
@@ -0,0 +1,5 @@
+# Here where I kept the bed files created from merging all the transcripts (genic) and subtracting it from the entire genome to get the non-genic regions
+# /media/behnam/Black_Seagate2/Mouse/Data/mm10hgTables_Genic.bed
+# /media/behnam/Black_Seagate2/Mouse/Data/mm10hgTables_NonGenic.bed
+
+
