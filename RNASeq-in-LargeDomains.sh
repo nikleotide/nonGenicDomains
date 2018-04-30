50e83ba3f05a4b540e8e9130cd1d7e0e96fc1ac6
Hamid Nikbakht <me@nikleotide.com>
2018-04-30 17:08:39 -0400

    Update RNASeq-in-LargeDomains.sh

--- RNASeq-in-LargeDomains.sh
+++ RNASeq-in-LargeDomains.sh
@@ -28,5 +28,40 @@ for file in *.bam;do echo $file;samtools flagstat $file | grep mapped | grep -e 
 #P2-3.sorted.bam
 55686982
 
-# Next, running bedtools multicov on all the bam files at the same time using the domains and subdomains (genic/nongenic) as the input bed file
+# Next, running bedtools multicov on all the bam files at the same time using the domains and subdomains (genic/nongenic) as the input bed file. This needs to be done for all WT-domain-marks
 
+## The bedtools are here:
+
+###### K9me3
+# LARGE K9me3
+/media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_3_Parental_Domains_in_4_Cells/Parental_K9me3_Domains_vs_Methylation_in_4_Cells/K9me3_Domains/H3K9me3-50kb-H3K9me3-1stComplete_Genome-peaks.-FILTERED_fixed.filtered_VALUES1.ALL.LARGE.bedgraph
+
+# NONGENIC LARGE K9me3
+/media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_3_Parental_Domains_in_4_Cells/Parental_K9me3_Domains_vs_Methylation_in_4_Cells/K9me3_Domains/H3K9me3-50kb-H3K9me3-1stComplete_Genome-peaks.-FILTERED_fixed.filtered_VALUES1.NONGENIC.LARGE.bedgraph
+
+# GENIC LARGE K9me3
+/media/behnam/Black_Seagate2/Mouse/Final_Figure1C/ANALYSIS/Methylation_in_3_Parental_Domains_in_4_Cells/Parental_K9me3_Domains_vs_Methylation_in_4_Cells/K9me3_Domains/H3K9me3-50kb-H3K9me3-1stComplete_Genome-peaks.-FILTERED_fixed.filtered_VALUES1.GENIC.LARGE.bedgraph
+
+
+###### K36me3
+# LARGE K36me3
+
+
+# NONGENIC K36me3
+
+
+# GENIC K36me3
+
+
+###### K27me3
+# LARGE K27me3
+
+
+# NONGENIC K27me3
+
+
+# GENIC K27me3
+
+
+
+bedtools multicov -q 50 -bed mm10_1kb_intervals.bed -bams
