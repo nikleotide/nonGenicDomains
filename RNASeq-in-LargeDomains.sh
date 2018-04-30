44ea1beed94984947c4d77da3d7fb6b0d17ab41a
Hamid Nikbakht <me@nikleotide.com>
2018-04-30 15:44:48 -0400

    Update RNASeq-in-LargeDomains.sh

--- RNASeq-in-LargeDomains.sh
+++ RNASeq-in-LargeDomains.sh
@@ -1,3 +1,30 @@
 # To calculate the RPKM in large domains in the 4 cell lines, first we need to know the number of mapped reads per sample.
 # go to the directory where you store rnaseq bam files with their index files and then:
 for file in *.bam;do echo $file;samtools flagstat $file | grep mapped | grep -e '(' | grep -e 'N/A' | awk '{print $1}';done
+
+#Here are the results:
+#B2-1.sorted.bam
+81126550
+#B2-2.sorted.bam
+54292876
+#B2-3.sorted.bam
+49819820
+#C22-1.sorted.bam
+72154994
+#C22-2.sorted.bam
+62585132
+#C22-3.sorted.bam
+54084870
+#K36M-1.sorted.bam
+74441204
+#K36M-2.sorted.bam
+55633502
+#K36M-3.sorted.bam
+44524276
+#P2-1.sorted.bam
+69928354
+#P2-2.sorted.bam
+51730034
+#P2-3.sorted.bam
+55686982
+
